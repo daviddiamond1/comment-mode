b2f3eb6f48ff00e465a2f70fb2498bbbc18b1d40
David Diamond <daviddiamond2@gmail.com>
2017-01-14 19:52:38 +0000

    Toggle between on/ off
    - disable script for turning off comment mode
    - new svg for when comment mode is enabled
    - responsiveness for when the player is resized and moved

---
+++ disable.js
@@ -0,0 +1,28 @@
+$(".comment-mode-enabled").click(function(){
+  $("#comment-mode-disable").css("display","none");
+  $("#comment-mode-enable").css("display","inline-block");
+  $("#player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("position","");
+  $("#player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","");
+  $("#player-api").removeClass('yt-card');
+  $(".player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("background","");
+  $(".player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","");
+  $(".player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("left","");
+  $("#watch7-sidebar-contents").removeAttr('www-core-2x-webp-vfl59ixOR').css("position","");
+  $("#watch7-sidebar-contents").removeAttr('www-core-2x-webp-vfl59ixOR').css("width", "");
+  $("#watch7-sidebar-contents").removeAttr('www-core-2x-webp-vfl59ixOR').css("top","");
+  $(".video-stream").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","");
+  $(".video-stream").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","");
+  $(".ytp-right-controls").removeAttr('www-core-2x-webp-vfl59ixOR').css("position","");
+  $(".ytp-right-controls").removeAttr('www-core-2x-webp-vfl59ixOR').css("left","");
+  $(".html5-video-player").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","");
+  $(".html5-main-video").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","");
+  $(".ytp-progress-bar-container").css("width","auto;");
+  $(".ytp-progress-list").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","");
+  $(".ytp-progress-bar").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","");
+  $("#watch7-main-container").removeAttr('www-core-2x-webp-vfl59ixOR').css("margin-top","");
+  $("#page").removeAttr('www-core-2x-webp-vfl59ixOR').css("padding-top","");
+  $(".ytp-size-button").removeAttr('www-player-2x-webp-vflgpIFfP').css("display","");
+  $(".html5-video-container").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","360px");
+  $("ytp-subtitles-button").removeAttr('www-core-2x-webp-vfl59ixOR').css("display", "inline-block");
+  $("ytp-subtitles-button").attr("disabled", "true");
+})(window, jQuery);
