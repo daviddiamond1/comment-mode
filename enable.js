8e15f36f1fc10101483083b86d70de2af57bc776
David Diamond <daviddiamond2@gmail.com>
2017-08-31 00:22:14 +0100

    First steps towards a fix

--- enable.js
+++ enable.js
@@ -5,30 +5,8 @@
 })(window, jQuery);
 
   $("#comment-mode-enable").click(function(){
-    $('#player-api').prependTo( $('.watch-sidebar-gutter') )
-    $("#comment-mode-disable").css("display","inline-block");
-    $("#comment-mode-enable").css("display","none");
-    $("#player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","231px");
-    $("#player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","410px");
-    $("#player-api").addClass('yt-card');
-    $(".player-api").removeAttr('www-core-2x-webp-vfl59ixOR').css("background","none");
-    $("#watch7-content").removeAttr('www-core-2x-webp-vfl59ixOR').css("margin-top","-380px");
-    $("#watch7-sidebar-contents").removeAttr('www-core-2x-webp-vfl59ixOR').css("position","fixed");
-    $("#watch7-sidebar-contents").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","422px");
-    $(".video-stream").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","410px");
-    $(".video-stream").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","240px");
-    $(".ytp-right-controls").removeAttr('www-core-2x-webp-vfl59ixOR').css("position","absolute");
-    $(".ytp-right-controls").removeAttr('www-core-2x-webp-vfl59ixOR').css("left","290px");
-    $(".html5-video-player").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","231px");
-    $(".html5-main-video").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","231px");
-    $(".ytp-progress-bar-container").css("width","410px;");
-    $(".ytp-progress-list").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","380px");
-    $(".ytp-progress-bar").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","380px");
-    $("#page").removeAttr('www-core-2x-webp-vfl59ixOR').css("padding-top","10px");
-    $(".ytp-size-button").removeAttr('www-player-2x-webp-vflgpIFfP').css("display","none");
-    $(".ytp-subtitles-button").removeAttr('www-core-2x-webp-vfl59ixOR').css("display", "none");
-    $(".ytp-fullscreen-button").removeAttr('www-core-2x-webp-vfl59ixOR').css("display", "none");
-    $(".watch-sidebar-gutter").removeAttr('www-core-2x-webp-vfl59ixOR').css("padding-top", "8px");
-    $(".watch-sidebar-gutter").removeAttr('www-core-2x-webp-vfl59ixOR').css("padding-right", "8px");
-    $(".watch-sidebar-gutter").removeAttr('www-core-2x-webp-vfl59ixOR').css("padding-left", "6px");
+    $('.html5-video-player').prependTo( $('#related') );
+    $("#player").removeAttr('www-core-2x-webp-vfl59ixOR').css("width","70%");
+    $("#player").removeAttr('www-core-2x-webp-vfl59ixOR').css("height","70%");
+    $("#related").removeAttr('www-core-2x-webp-vfl59ixOR').css("margin-top","35%");
   })(window, jQuery);
